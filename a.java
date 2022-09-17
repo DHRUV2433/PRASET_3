0a0d9615db41c06660b66c7cfebd093ef21bf181
DHRUV2433 <109072264+DHRUV2433@users.noreply.github.com>
2022-09-17 15:48:04 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,12 @@
+//This program is run by Dhruv Vekariya(21CE152)
+package A;
+import B.b;
+public class a {
+    public static void main(String[] args) {
+        b objB = new b();
+        objB.method2();
+    }
+    public void method1(){
+        System.out.println("This is method1 of package A");
+    }
+}
---
+++ b.java
@@ -0,0 +1,12 @@
+//This program is run by Dhruv Vekariya(21CE152)
+package B;
+import A.a;
+public class b {
+    public static void main(String[] args) {
+        a objA=new a();
+        objA.method1();
+    }
+    public void method2(){
+        System.out.println("This is method2 of package B");
+    }
+}
