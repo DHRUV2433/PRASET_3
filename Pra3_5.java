2aaa0b061bba30271bbaeb1a9c13f40c4b3b2292
DHRUV2433 <109072264+DHRUV2433@users.noreply.github.com>
2022-09-17 15:49:50 +0530

    Add files via upload

---
+++ Pra3_5.java
@@ -0,0 +1,12 @@
+//This program is run by Dhruv Vekariya(21CE152)
+package University;
+import Institute.pra3_5;
+public class Pra3_5 {
+    public static void main(String[] args) {
+        pra3_5 objI = new pra3_5();
+        objI.method2();
+    }
+    public void method1(){
+        System.out.println("This is method 1 of University package");
+    }
+}
---
+++ pra3_5.java
@@ -0,0 +1,12 @@
+//This program is run by Dhruv Vekariya(21CE152)
+package Institute;
+import University.Pra3_5;
+public class pra3_5 {
+    public static void main(String[] args) {
+        Pra3_5 objU=new Pra3_5();
+        objU.method1();
+    }
+    public void method2(){
+        System.out.println("This is method 2 of Institute package");
+    }
+}
